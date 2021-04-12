634bceb563f79989dc0df62fe4bf0765e283d0a7
Simon Paul <simonpaul@mailbox.org>
2021-04-12 21:11:32 +0200

    -

--- tui/idgames.go
+++ tui/idgames.go
@@ -1,9 +1,17 @@
 package tui
 
-import (
-	"github.com/zmnpl/goidgames"
-)
+import "github.com/rivo/tview"
+
+//"github.com/zmnpl/goidgames"
+
+type IdgamesBrowser struct {
+	layout      *tview.Grid
+	list        *tview.Table
+	fileDetails *tview.Table
+	reviews     *tview.Table
+	search      *tview.InputField
+}
 
 func init() {
-	goidgames.Get(1338, "")
+	//goidgames.Get(1338, "")
 }
